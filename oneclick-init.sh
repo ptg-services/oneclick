1e6cb2987a193d0cff565648f060ce62283cbc51
hshira <hshira@gmail.com>
2017-12-08 16:12:52 -0600

    Update oneclick-init.sh

--- oneclick-init.sh
+++ oneclick-init.sh
@@ -1,13 +1,14 @@
-    echo "Initializing oneclick"
-    ONECLICK_DIR=~/.oneclick
 
-    if [ -d "$ONECLICK_DIR" ]
-    then
-        echo "$ONECLICK_DIR directory  exists!"
-        cd $ONECLICK_DIR
-        git pull
-    else
-        mkdir -p $ONECLICK_DIR
-        git clone https://github.intuit.com/PTG/oneclick.git $ONECLICK_DIR
-        echo "Successfully cloned"
-    fi
+echo "Initializing oneclick"
+ONECLICK_DIR=~/.oneclick
+
+if [ -d "$ONECLICK_DIR" ]
+then
+    echo "$ONECLICK_DIR directory  exists!"
+    cd $ONECLICK_DIR
+    git pull
+else
+    mkdir -p $ONECLICK_DIR
+    git clone https://github.intuit.com/PTG/oneclick.git $ONECLICK_DIR
+    echo "Successfully cloned"
+fi
